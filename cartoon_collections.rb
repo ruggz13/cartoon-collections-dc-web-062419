dd14d63c758db50021036924daf2d4bb8ec05942
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-15 17:02:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.collect {|call| call + "!"}
 end
 
 def long_planeteer_calls# code an argument here
