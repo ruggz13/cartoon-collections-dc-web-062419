113193e56a9cbac4dc9191eae918eebcb6ff80ed
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-15 17:03:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(calls)# code an argument here
   # Your code here
-  calls.collect {|call| call + "!"}
+  calls.map {|call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
